bc120f71d6412a4aedd932dfd22ba8d0f6da17b8
RziguiMohamedAmine <62619234+RziguiMohamedAmine@users.noreply.github.com>
2023-10-05 11:09:51 +0100

    test

--- DevOps_Project/src/main/java/tn/esprit/devops_project/services/StockServiceImpl.java
+++ DevOps_Project/src/main/java/tn/esprit/devops_project/services/StockServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class StockServiceImpl implements IStockService {
- 
+
    private final StockRepository stockRepository;
 
     @Override
