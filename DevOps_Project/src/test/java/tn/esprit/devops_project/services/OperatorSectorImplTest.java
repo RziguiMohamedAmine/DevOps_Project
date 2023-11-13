10b882076d278f08145886ec69d41b488a2af328
Zied Mabrouk <84295527+Zied-Mabrouk@users.noreply.github.com>
2023-11-13 23:42:52 +0100

    fix

--- DevOps_Project/src/test/java/tn/esprit/devops_project/services/OperatorSectorImplTest.java
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/OperatorSectorImplTest.java
@@ -34,119 +34,14 @@ class OperatorImplTest {
     }
 
     @Test
-    void testretrieveAllOperators() {
-
-        List<Operator> Operators = Arrays.asList(new Operator(), new Operator());
-
-        // Mock the behavior of the repository
-        when(OperatorRepository.findAll()).thenReturn(Operators);
-
-        // Call the service method
-        List<Operator> result = Operatorservice.retrieveAllOperators();
-
-        // Verify that the repository method was called and the result is as expected
-        verify(OperatorRepository).findAll();
-        assertEquals(2, result.size());
-
-    }
-
-    @Test
-    void addOperator() {
-
-        Operator Operator = new Operator();
-
-        // Mock the behavior of the repository
-        when(OperatorRepository.save(Operator)).thenReturn(Operator);
-
-        // Call the service method
-        Operator result = Operatorservice.addOperator(Operator);
-
-        // Verify that the repository method was called and the result is as expected
-        verify(OperatorRepository).save(Operator);
-        assertEquals(Operator, result);
-
-
-
-    }
-
-    @Test
-    void deleteOperator() {
-
+    public void whenValidId_thenOperatorShouldBeFound() {
         Long id = 1L;
+        Operator operator = new Operator(/* parameters */);
+        Mockito.when(OperatorRepository.findById(id)).thenReturn(Optional.of(operator));
 
-        // Mock the behavior of the repository's deleteById method
-        doNothing().when(OperatorRepository).deleteById(id);
+        Operator found = Operatorservice.retrieveOperator(id);
 
-        // Call the service method
-        Operatorservice.deleteOperator(id);
-
-        // Verify that the repository's deleteById method was called
-        verify(OperatorRepository).deleteById(id);
-
-
+        assertEquals(found.getIdOperateur(),id);
     }
 
-    @Test
-    void updateOperator() {
-
-        Operator Operator = new Operator();
-
-        // Mock the behavior of the repository's save method
-        when(OperatorRepository.save(Operator)).thenReturn(Operator);
-
-        // Call the service method
-        Operator result = Operatorservice.updateOperator(Operator);
-
-        // Verify that the repository's save method was called and the result is as expected
-        verify(OperatorRepository).save(Operator);
-        assertEquals(Operator, result);
-
-
-
-    }
-
-    @Test
-    void retrieveOperator() {
-        Operator Operator = new Operator();
-        Long id = 1L;
-
-        // Mock the behavior of the repository
-        when(OperatorRepository.findById(id)).thenReturn(Optional.of(Operator));
-
-        // Call the service method
-        Operator result = Operatorservice.retrieveOperator(id);
-
-        // Verify that the repository method was called and the result is as expected
-        verify(OperatorRepository).findById(id);
-        assertEquals(Operator, result);
-
-
-    }
-
-    @Test
-    void testUpdateOperator_NonExisting() {
-        Operator Operator = new Operator();
-
-        when(OperatorRepository.save(Operator)).thenThrow(new IllegalArgumentException("Operator does not exist."));
-
-        assertThrows(IllegalArgumentException.class, () -> {
-            Operatorservice.updateOperator(Operator);
-        });
-
-        verify(OperatorRepository).save(Operator);
-    }
-
-
-    @Test
-    void testDeleteOperator_NonExisting() {
-        Long invalidId = 999L;
-
-        doThrow(new IllegalArgumentException("Operator does not exist.")).when(OperatorRepository).deleteById(invalidId);
-
-        assertThrows(IllegalArgumentException.class, () -> {
-            Operatorservice.deleteOperator(invalidId);
-        });
-
-        verify(OperatorRepository).deleteById(invalidId);
-    }
 }
